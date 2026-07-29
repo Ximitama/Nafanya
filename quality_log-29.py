d51c5ae8c52b59e8fd7d87f7eea175d77d6dce08
IVAN <dollemmani@outlook.com>
2026-07-29 21:47:24 +0200

    Stage 29: Добавь конфигурацию приложения через словарь настроек

---
+++ quality_log-29.py
@@ -0,0 +1,9 @@
+# === Stage 29: Добавь конфигурацию приложения через словарь настроек ===
+# Project: QualityLog
+APP_CONFIG = {
+    "app_name": "QualityLog",
+    "version": "29.0",
+    "log_dir": "./logs",
+    "max_history": 100,
+    "default_responder": None,
+}
